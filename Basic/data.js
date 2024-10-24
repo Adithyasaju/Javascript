151111f23606111684b0fbe6cb95a577e397457f
Adithyasaju <adithyasajuo7oaaa2gmail.com>
2024-10-24 19:19:01 +0530

    Learn

---
+++ Basic/data.js
@@ -0,0 +1,8 @@
+var eid=101
+var ename="Rahul"
+
+/*console.log(eid);
+console.log(ename);*/                  //Output displayed in console
+
+document.write(eid)
+document.write(ename)                 //Output displayed in web page
